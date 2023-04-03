eb0ac9e4cf4f4c7ad42473b18f24002309074ddc
Priestly Patrick bassey <pbassey30@gmail.com>
2023-04-03 22:59:44 +0100

    more css modification on book page

--- src/pages/book-mass/book_mass.js
+++ src/pages/book-mass/book_mass.js
@@ -12,42 +12,39 @@ function Bookmass() {
             <div className='row bm_row'>
                 <div className=' row col-lg-6'>
 
-                    <h1>Book Mass</h1>
+                    <h1 className='fw-bold'>Book Mass</h1>
 
                     <p>Please make your payment to the below account and upload your Receipt, thank you for compliance.</p>
 
                     <strong>Bank: <span>Wema Bank</span></strong>
                     <strong>Account: <span>0434908920</span></strong>
 
-                    <div className='col-lg-6 mt-5' data-aos="fade-up" data-aos-duration="1000">
-
-                        <input type="text" name="text" class="input w-100" placeholder="Family name"></input>
+                    <div className='col-lg-6 mt-5' data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
+                        <label>Family name</label>
+                        <input type="text" name="text" className="input w-100"></input>
                     </div>
 
-                    <div className='col-lg-6 mt-5' data-aos="fade-up" data-aos-duration="1000">
+<div className='col-lg-6 mt-5' data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
+    <label>Email</label>
+    <input type="text" name="text" className="input w-100"></input>
+</div>
 
-                        <input type="text" name="text" class="input w-100" placeholder="Email"></input>
-                    </div>
+<div className='col-lg-6 mt-2' data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
+    <label>Date</label>
+    <input type="datetime-local" name="check-out" min="2000-01-01T00:00" max="2500-06-14T00:00" className="input w-100"></input>
+</div>
 
-                    <div className='col-lg-6 mt-2' data-aos="fade-up" data-aos-duration="1000">
+<div className='col-lg-6 mt-2' data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
+    <label>Reciept Please</label>
+    <input type="file" name="text" className="input w-100"></input>
+</div>
 
-                        <input type="datetime-local" id="check-out" name="check-out" min="2000-01-01T00:00" max="2500-06-14T00:00" className='input w-100'></input>
-                    </div>
+<div className='col-12 mt-2' data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
+    <label>Intentions</label>
+    <textarea type="text" className="input w-100 h-50"></textarea>
+</div>
 
-                    <div className='col-lg-6 mt-2' data-aos="fade-up" data-aos-duration="1000">
-
-                        <input type="file" name="text" class="input w-100"></input>
-                    </div>
-
-                    <div className='col-12' data-aos="fade-up" data-aos-duration="600">
-                        <div className="bm_input-container">
-                            <textarea className="bm_input" placeholder='Write your intention' name="text" />
-                            <div className="topline"></div>
-                            <div className="underline"></div>
-                        </div>
-                    </div>
-
-                    <div className='bm_btn_cover' data-aos="fade-up" data-aos-duration="500">
+                    <div className='bm_btn_cover' data-aos="fade-up" data-aos-duration="900">
                         <button className="bm_btn">
                             <span className="bm_btn-text-one">Book now</span>
                             <span className="bm_btn-text-two">Great!</span>
