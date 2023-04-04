4a348a70b3555ac67e6fbfa99493baa0075b259d
Priestly Patrick bassey <pbassey30@gmail.com>
2023-04-04 12:09:07 +0100

    still the book page, it's stressing me

--- src/pages/book-mass/book_mass.js
+++ src/pages/book-mass/book_mass.js
@@ -18,43 +18,30 @@ function Bookmass() {
 
                     <strong>Bank: <span>Wema Bank</span></strong>
                     <strong>Account: <span>0434908920</span></strong>
-                    
 
-                    <div className='col-lg-6 mt-3' data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
-                        <label>Family name</label>
-                        <br />
-                        <input type="text" name="text" className="input w-100"></input>
-                    </div>
 
-<div className='col-lg-6 mt-3' data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
-    <label>Email</label>
-                        <br />
-    <input type="text" name="text" className="input w-100"></input>
-</div>
+                    <div className='col-lg-6 mt-3'>
 
-<div className='col-lg-6 mt-3' data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
-    <label>Date</label>
-                        <br />
-    <input type="datetime-local" name="check-out" min="2000-01-01T00:00" max="2500-06-14T00:00" className="input w-100"></input>
-</div>
+                        <div class="subscribe">
+                            <input placeholder="Family name" class="subscribe-input" name="Family name" type="text" data-aos="fade-up" data-aos-duration="600" data-aos-delay="2" />
+                            <input placeholder="Your e-mail" class="subscribe-input" name="email" type="email" data-aos="fade-up" data-aos-duration="600" data-aos-delay="3" />
+                            <div>
+                            <label>Date For Mass</label>
+                            <input placeholder="Date" class="subscribe-input" type="datetime-local"  name="Date" min="2000-01-01T00:00" max="2500-06-14T00:00" data-aos="fade-up" data-aos-duration="800" data-aos-delay="4" />
+                            </div>
+                            <div>
+                            <label>Upload Receipt</label>
+                            <input accept=".jpg, .jpeg, .png, .gif, .pdf" class="subscribe-input" name="arquivo" type="file" data-aos="fade-up" data-aos-duration="800" data-aos-delay="5" />
+                            </div>
+                            <textarea placeholder="Write your Intention" class="subscribe-textarea" name="email" type="email" data-aos="fade-up" data-aos-duration="800" data-aos-delay="3" />
+                            <div class="submit-btn" data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
+                                <button className="bm_btn">
+                                    <span className="bm_btn-text-one">Book now</span>
+                                    <span className="bm_btn-text-two">Great!</span>
+                                </button>
+                            </div>
+                        </div>
 
-<div className='col-lg-6 mt-3' data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
-    <label>Reciept Please</label>
-                        <br />
-    <input type="file" name="text" className="input w-100"></input>
-</div>
-
-<div className='col-12 mt-3' data-aos="fade-up" data-aos-duration="900" data-aos-delay="2">
-    <label>Intentions</label>
-                        <br />
-    <textarea type="text" className="input w-100 h-50"></textarea>
-</div>
-
-                    <div className='bm_btn_cover' data-aos="fade-up" data-aos-duration="900">
-                        <button className="bm_btn">
-                            <span className="bm_btn-text-one">Book now</span>
-                            <span className="bm_btn-text-two">Great!</span>
-                        </button>
                     </div>
 
                 </div>
