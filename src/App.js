4733dbcb7dfe41c06aa2c31091268bc0c852bb94
Priestly Patrick bassey <pbassey30@gmail.com>
2025-05-28 14:53:42 +0100

    Hmm, Commented another register call at app.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import EventImgDataadd from "./component/eventimg_d_btn/eventimg_d_add";
 import EventsAdd from "./pages/events/events_add";
 import Aos from "aos"
 import Bookmass from "./pages/book-mass/book_mass";
-import Register from "./pages/register/Register";
+// import Register from "./pages/register/Register";
 import UpdatePrayer from "./pages/prayer/updatePrayer";
 import UpdateNews from "./pages/news/updatenews";
 
@@ -40,7 +40,7 @@ function App() {
           <Routes>
             <Route path='/' exact element={<Layout />} />
             <Route path="/admin" element={user ? <AdminPage /> : <Login />} />
-            <Route path="/register" element={<Register />} />
+            {/*<Route path="/register" element={<Register />} />*/}
             <Route path="/about" element={<About />} />
             <Route path="/book&mass" element={<Bookmass />} />
             <Route path="/prayers" element={<Prayer />} />
